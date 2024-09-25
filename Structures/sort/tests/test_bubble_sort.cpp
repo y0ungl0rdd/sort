dda4ebfb05e94048445bfefb9f25bfe64f79906d
y0ungl0rdd <y4ngl0rddz@gmail.com>
2024-09-26 00:00:49 +0300

    Add files via upload

---
+++ Structures/sort/tests/test_bubble_sort.cpp
@@ -0,0 +1,25 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include "bubble_sort.h"
+
+TEST_CASE() {
+	int n = 5;
+
+	int array[5] = { 2, 4, 9, 8, 3 };
+	BubbleSort(n, array);
+	for (int i = 0; i < n - 1; i++) {
+		REQUIRE(array[i] <= array[i + 1]);
+	}
+
+	int array1[5] = {};
+	BubbleSort(n, array1);
+	for (int i = 0; i < n - 1; i++) {
+		REQUIRE(array1[i] <= array1[i + 1]);
+	}
+
+	float array2[5] = { 3.3, 1.5, -3, 10, 4 };
+	BubbleSort(n, array2);
+	for (int i = 0; i < n - 1; i++) {
+		REQUIRE(array2[i] <= array2[i + 1]);
+	}
+}
---
+++ Structures/sort/tests/test_selection_sort.cpp
@@ -0,0 +1,25 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include "selection_sort.h"
+
+TEST_CASE() {
+	int n = 5;
+
+	int array[5] = { 2, 4, 9, 8, 3 };
+	SelectionSort(n, array);
+	for (int i = 0; i < n - 1; i++) {
+		REQUIRE(array[i] <= array[i + 1]);
+	}
+
+	int array1[5] = {};
+	SelectionSort(n, array1);
+	for (int i = 0; i < n - 1; i++) {
+		REQUIRE(array1[i] <= array1[i + 1]);
+	}
+
+	float array2[5] = { 3.3, 1.5, -3, 10, 4 };
+	SelectionSort(n, array2);
+	for (int i = 0; i < n - 1; i++) {
+		REQUIRE(array2[i] <= array2[i + 1]);
+	}
+}
